80e15ff8df06389a7af173241d7dabefa7467993
= <abdallaroom25@gmail.com>
2025-11-03 16:24:59 +0200

    make the script run in the shell

--- session.js
+++ session.js
@@ -70,7 +70,7 @@ const YOUR_PASSWORD = 'Az01027101373@#';
 
 async function loginAndGetCookies() {
   const browser = await puppeteer.launch({ 
-    headless: true,
+    headless: "shell",
     defaultViewport: null,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
